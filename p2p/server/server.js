11793508d44ecb204b968ad9b124e309d992ea08
leon.xu <leon.xu@ringcentral.com>
2023-07-18 17:33:47 +0800

    structure

--- p2p/server/server.js
+++ p2p/server/server.js
@@ -1,53 +1,32 @@
-var log4js = require('log4js');
+
 var http = require('http');
 var https = require('https');
-var fs = require('fs');
 var socketIo = require('socket.io');
 
 var express = require('express');
-var serveIndex = require('serve-index');
 
 var USERCOUNT = 3;
 
-log4js.configure({
-    appenders: {
-        file: {
-            type: 'file',
-            filename: 'app.log',
-            layout: {
-                type: 'pattern',
-                pattern: '%r %p - %m',
-            }
-        }
-    },
-    categories: {
-        default: {
-            appenders: ['file'],
-            level: 'debug'
-        }
-    }
-});
-
-var logger = log4js.getLogger();
-
 var app = express();
-app.use(serveIndex('./client'));
 app.use(express.static('./client'));
 
-
+app.get("/", (req, res) => {
+    res.redirect(`/index.html`);
+  });
 
 //http server
 var http_server = http.createServer(app);
 http_server.listen(80, '0.0.0.0');
 
-var options = {
-        key : fs.readFileSync('./cert/rtcmedia.top.key'),
-        cert: fs.readFileSync('./cert/rtcmedia.top_bundle.pem')
-}
+// var options = {
+//         key : fs.readFileSync('./cert/rtcmedia.top.key'),
+//         cert: fs.readFileSync('./cert/rtcmedia.top_bundle.pem')
+// }
 
 //https server
-var https_server = https.createServer(options, app);
-var io = socketIo.listen(https_server);
+// var https_server = https.createServer(options, app);
+var https_server = https.createServer(app);
+var io = socketIo(https_server);
 
 
 io.sockets.on('connection', (socket)=> {
@@ -60,7 +39,7 @@ io.sockets.on('connection', (socket)=> {
             socket.join(room);
             var myRoom = io.sockets.adapter.rooms[room];
             var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
-            logger.debug('the user number of room is: ' + users);
+            //logger.debug('the user number of room is: ' + users);
 
             if(users < USERCOUNT){
                     socket.emit('joined', room, socket.id, users > 1); //发给除自己之外的房间内的所有人
@@ -79,7 +58,7 @@ io.sockets.on('connection', (socket)=> {
     socket.on('leave', (room)=>{
         var myRoom = io.sockets.adapter.rooms[room];
         var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
-        logger.debug('the user number of room is: ' + (users-1));
+        //logger.debug('the user number of room is: ' + (users-1));
         //socket.emit('leaved', room, socket.id);
         //socket.broadcast.emit('leaved', room, socket.id);
         socket.to(room).emit('bye', room, socket.id);
